095fe8856bd480c5808fbcedc1727f701db83825
Alen Troyan <alentroyan@gmail.com>
2025-05-27 08:42:41 +0300

    Create family saction

---
+++ js/swiper_import.js
@@ -0,0 +1,32 @@
+// Импорт Swiper и его модулей
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+// Импорт стилей (обязательно!)
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
